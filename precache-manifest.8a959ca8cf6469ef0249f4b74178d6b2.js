8ca0904d5bf3de3c886179a72e3925b77bbb9cc0
ltnsunrise <ltn.sunrise@gmail.com>
2019-08-24 19:33:42 +0700

    Updates

---
+++ precache-manifest.8a959ca8cf6469ef0249f4b74178d6b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa9733ce72803444f86b7bd9b7263f1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a922f4da0d7553134077",
+    "url": "/monsters-rolodex/static/css/main.cb6a21ac.chunk.css"
+  },
+  {
+    "revision": "e6f182edb81f551cb0c1",
+    "url": "/monsters-rolodex/static/js/2.a4cf3094.chunk.js"
+  },
+  {
+    "revision": "a922f4da0d7553134077",
+    "url": "/monsters-rolodex/static/js/main.03b25836.chunk.js"
+  },
+  {
+    "revision": "c7b84e328d40a43989ce",
+    "url": "/monsters-rolodex/static/js/runtime~main.45a8f4a3.js"
+  }
+]);
